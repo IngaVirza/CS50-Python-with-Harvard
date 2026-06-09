a9b2ce54385d324aaeb926e2a2c7edbd0f9edfd3
unknown <inha.virza@gmail.com>
2026-06-09 19:22:15 +0300

    cleaner function for parity with if, else

--- parity.py
+++ parity.py
@@ -9,11 +9,11 @@ def main():
 
 
 def is_even(n):
-    if n % 2 == 0:
-        return True
-    else:
-        return False
-
+   #if n % 2 == 0:
+   #return True
+    #else:
+     #  return False
+    return n % 2 == 0
 
 main()
 
