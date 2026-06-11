0faad9d1317f2997427973332b8a123f1c59b1d3
unknown <inha.virza@gmail.com>
2026-06-11 23:20:36 +0300

    Dictionaries

---
+++ report.py
@@ -0,0 +1,19 @@
+def main():
+    spacecraft = {"name": "Voyager 1"}
+    spacecraft.update({"distance": 0.01, "orbit": "Sun"})
+    print(create_report(spacecraft))
+
+
+def create_report(spacecraft):
+    return f""""
+
+    ======== REPORT ===============
+    Name: {spacecraft.get("name", "Unknown")}
+    Distance: {spacecraft.get("distance", "Unknown")}
+    Orbit: {spacecraft.get("orbit")}
+
+    ===============================
+    """
+
+
+main()
