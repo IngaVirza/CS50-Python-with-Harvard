9d63c1d386c87e969fd4f2abe35ec8e463e26d80
unknown <inha.virza@gmail.com>
2026-06-19 21:19:46 +0300

    Unit-test with PYTEST(update)

--- test_calculator.py
+++ test_calculator.py
@@ -34,11 +34,22 @@ from calculator1 import square
 # 
 
 
-def test_square():
+# def test_square():
+#     assert square(2) == 4
+#     assert square(3) == 9
+#     assert square(-2) == 4
+#     assert square(-3) == 9
+#     assert square(0) == 0
+
+
+
+def test_positive():
     assert square(2) == 4
     assert square(3) == 9
+
+def test_negative():
     assert square(-2) == 4
     assert square(-3) == 9
-    assert square(0) == 0
 
-    
+def test_zero():
+    assert square(0) == 0        
