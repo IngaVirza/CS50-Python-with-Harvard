f9b8251ba78e57aa08e65eccb773b152352f39b1
unknown <inha.virza@gmail.com>
2026-06-19 21:31:46 +0300

    Unit-test with pytest.raises(TypeError))

--- test_calculator.py
+++ test_calculator.py
@@ -1,3 +1,4 @@
+import pytest
 from calculator1 import square
 
 
@@ -53,3 +54,8 @@ def test_negative():
 
 def test_zero():
     assert square(0) == 0        
+
+
+def test_str():
+    with pytest.raises(TypeError):
+        square("cat")
