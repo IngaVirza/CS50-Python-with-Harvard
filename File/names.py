efd060b2c18307a106d1fc9679dc19b3de27ad29
unknown <inha.virza@gmail.com>
2026-06-21 00:54:24 +0300

    File I/O with open  as file

--- File/names.py
+++ File/names.py
@@ -2,6 +2,10 @@ name = input("What's your name?")
 
 
 # file = open("name.txt", "w")
-file = open("name.txt", "a")
-file.write(f"{name}\n")
-file.close()
+# file = open("name.txt", "a")
+# file.write(f"{name}\n")
+# file.close()
+
+
+with open("names.txt", "a") as file:
+    file.write(f"{name}\n")
