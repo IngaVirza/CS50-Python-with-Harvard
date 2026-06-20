15d86075148e0d6ffcc90cc6865f3394c9c83c29
unknown <inha.virza@gmail.com>
2026-06-21 01:05:07 +0300

    File I/O with open and file.readlines(update)

--- File/names.py
+++ File/names.py
@@ -12,7 +12,7 @@
 
 
 with open("names.txt", "r") as file:
-    lines = file.readlines()
+    # lines = file.readlines()
 
-for line in lines:
-    print("hello,", line.rstrip())    
+    for line in file:
+        print("hello,", line.rstrip())    
