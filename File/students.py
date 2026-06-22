c4be35144de4d78bc1a936665b4caf26f4ecd011
unknown <inha.virza@gmail.com>
2026-06-22 20:29:25 +0300

    File I/O with csv.DictWriter(update)

--- File/students.py
+++ File/students.py
@@ -60,5 +60,5 @@ name = input("What's your name?")
 home = input("Where's your home?")
 
 with open("students.csv", "a") as file:
-    writer = csv.DictWriter(file, fildnames=["name", "home"])
-    writer.writerow({"name": name, "home": home})
+    writer = csv.DictWriter(file, fieldnames=["name", "home"])
+    writer.writerow({"home": home, "name": name})
