93ad82bfb924d756ad48bfa2711697bb19ec537d
unknown <inha.virza@gmail.com>
2026-06-22 19:40:30 +0300

    File I/O with csv file with sorted in dictionary(update with lambda

--- File/students.py
+++ File/students.py
@@ -32,5 +32,5 @@ with open("students.csv") as file:
 
 
 
-for student in sorted(students, key=lambda, student: student["name"]): 
+for student in sorted(students, key=lambda student: student["name"]): 
     print(f"{student['name']} is in {student['house']}")
