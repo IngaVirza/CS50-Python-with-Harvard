19817878d442400865420923e999256ac98e96d5
unknown <inha.virza@gmail.com>
2026-06-22 17:21:19 +0300

    File I/O with open and file.readlines(update2 with sorted)

--- File/names.py
+++ File/names.py
@@ -21,14 +21,16 @@
 
 
 
-names = []
+# names = []
+
+# with open("names.txt") as file:
+#     for line in file:
+#         names.append(line.rstrip())
+
+# for name in sorted(names):
+#     print(f"hello, {name}")        
+
 
 with open("names.txt") as file:
-    for line in file:
-        names.append(line.rstrip())
-
-for name in sorted(names):
-    print(f"hello, {name}")        
-
-
-        
+    for line in sorted(file):
+        print("hello,", line.rstrip())        
