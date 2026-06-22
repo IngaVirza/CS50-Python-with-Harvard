03fb33ee3e770a77e7f67acef962c79f13727dcf
unknown <inha.virza@gmail.com>
2026-06-22 12:22:52 +0300

    File I/O with open and file.readlines(update with sorted)

--- File/names.py
+++ File/names.py
@@ -11,8 +11,24 @@
 #     file.write(f"{name}\n")
 
 
-with open("names.txt", "r") as file:
-    # lines = file.readlines()
 
+
+# with open("names.txt", "r") as file:
+#     # lines = file.readlines()
+
+#     for line in file:
+#         print("hello,", line.rstrip())    
+
+
+
+names = []
+
+with open("names.txt") as file:
     for line in file:
-        print("hello,", line.rstrip())    
+        names.append(line.rstrip())
+
+for name in sorted(names):
+    print(f"hello, {name}")        
+
+
+        
