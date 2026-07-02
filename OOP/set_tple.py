f4528161fecd4e391630ef60666c4d8fab87658f
unknown <inha.virza@gmail.com>
2026-07-02 23:34:52 +0300

    OOP with tuple and set(update)

--- OOP/set_tple.py
+++
@@ -1,5 +0,0 @@
-my_tuple = ("Junior", "Middle", "Senior", [1, 2, 3])
-
-my_tuple[-1].append(4)
-
-print(my_tuple)
---
+++ OOP/set_tuple.py
@@ -0,0 +1,18 @@
+my_tuple = ("Junior", "Middle", "Senior", [1, 2, 3])
+
+my_tuple[-1].append(4)
+
+print(my_tuple)
+# ('Junior', 'Middle', 'Senior', [1, 2, 3, 4])
+
+my_tuple1 = ("Junior", "Middle", "Senior")
+
+junior, middle, senior = my_tuple1
+
+print(junior)
+print(middle)
+print(senior)
+
+# Junior
+# Middle
+# Senior
