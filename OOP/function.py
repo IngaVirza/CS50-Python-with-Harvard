b4da3a74db234ba0eaf3e5103457815e9908b267
unknown <inha.virza@gmail.com>
2026-07-03 14:08:03 +0300

    function(update)

--- OOP/function.py
+++ OOP/function.py
@@ -2,9 +2,13 @@ def draw_symbol(text, count):
     print(text * count)
 
 
-draw_symbol('*', 5)
+# draw_symbol('*', 5)
 
-draw_symbol('!', 2)
+# draw_symbol('!', 2)
 
-draw_symbol('$', 10)
+# draw_symbol('$', 10)
 
+symbol= '*'
+count = 5
+
+draw_symbol(symbol, count)
