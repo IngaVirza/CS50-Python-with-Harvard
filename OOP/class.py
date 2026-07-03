3cb571697ace1b7fcbe858fd43f05808b24f5ffe
unknown <inha.virza@gmail.com>
2026-07-04 01:04:17 +0300

    class(update)

--- OOP/class.py
+++ OOP/class.py
@@ -1,7 +1,11 @@
 class User:
-    def __init__(self):
+    def __init__(self, name, age):
     
-        self.name = "Kirill"
+        self.name = name
+        self.age = age
+        self.is_human = True
 
-user1 = User() #Экземпляр
+user1 = User("Kirill",18) #Экземпляр
 user2 = User("Alexander", 32)
+
+print(user1.__dict__)
