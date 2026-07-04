c6a4c6a66c4d21cfb3dfda59ce2f8d44876b9cea
unknown <inha.virza@gmail.com>
2026-07-04 23:43:20 +0300

    Inheritance(update)

--- OOP/inheritance.py
+++ OOP/inheritance.py
@@ -1,3 +1,18 @@
+class Employee:
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+
+    def work(self):
+        print(f"{self.name} work")    
+
+class TesterQA(Employee):
+    pass       
+
+tester = TesterQA("Ayia", "300k")
+tester.work()
+        
+
 class Developer:
     def __init__(self, name, salary):
         self.name = name
