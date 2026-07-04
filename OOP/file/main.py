736c36b13dc59999b9b6e4f32d99efd6838e4454
unknown <inha.virza@gmail.com>
2026-07-05 00:35:47 +0300

    files(update2)

--- OOP/file/main.py
+++ OOP/file/main.py
@@ -1,6 +1,12 @@
 
 file = open('data.txt','r')
-try:
+# try:
+#     print(file.read())
+# finally:
+#     file.close()
+
+
+# Better
+
+with open('data.txt', 'r'):
     print(file.read())
-finally:
-    file.close()
