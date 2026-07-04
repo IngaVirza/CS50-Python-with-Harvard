ced6c170426ec895c2be56922bb4b0a4c2bb06a1
unknown <inha.virza@gmail.com>
2026-07-04 23:47:50 +0300

    Inheritance(update2)

--- OOP/inheritance.py
+++ OOP/inheritance.py
@@ -13,25 +13,22 @@ tester = TesterQA("Ayia", "300k")
 tester.work()
         
 
-class Developer:
-    def __init__(self, name, salary):
-        self.name = name
-        self.salary = salary
-
+class Developer(Employee):
+    def __init__(self, name, salary, language: str):
+        super().__init__(name, salary)
+        self.language = language
+     
     def work(self):
         print(f"{self.name} write code")
 
 
-class Designer:
-    def __init__(self, name, salary):
-        self.name = name
-        self.salary = salary
+class Designer(Employee):
 
     def work(self):
         print(f"{self.name} create interface")    
 
 
-developer = Developer("Inga", 32)
+developer = Developer("Inga", 32, language="Python")
 developer.work()
 
 designer = Designer("Max", 38)
