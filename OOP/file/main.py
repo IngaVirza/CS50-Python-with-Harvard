09c7734805730bb6138860c50dae3d4e1db18513
unknown <inha.virza@gmail.com>
2026-07-05 01:09:50 +0300

    files(update2)

--- OOP/file/main.py
+++ OOP/file/main.py
@@ -10,3 +10,20 @@ file = open('data.txt','r')
 
 with open('data.txt', 'r') as file:
     print(file.read())
+
+class FileOpener:
+    def __init__(self, file_path: str, mode:str):
+        self.file_path = file_path
+        self.mode = mode
+
+    def __enter__(self):
+        file = open(self.file_path, self.mode)
+        return file
+
+    def __exit__(self):
+        pass
+
+file_opener = FileOpener('data.txt','r')
+
+with FileOpener('data.txt', 'r') as file:
+    print(file.read())
