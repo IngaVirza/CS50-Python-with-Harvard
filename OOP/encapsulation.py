01d1a1dbc040cad5e01982a99eb0f54d8e24a97e
unknown <inha.virza@gmail.com>
2026-07-04 23:25:18 +0300

    Encapsulation(update)

--- OOP/encapsulation.py
+++ OOP/encapsulation.py
@@ -1,10 +1,16 @@
 class BankAccount:
     def __init__(self, balance):
-        self.balance = balance
+        self.__balance = balance
 
+    def deposit(self, amount):
+        if amount > 0:
+            self.__balance += amount
+            
+    def get_balance(self):
+        return self.__balance        
 
 bank = BankAccount(1000)
 
-print(bank.balance)
-bank.balance = 10000000
-print(bank.balance)
+print(bank.__balance)
+bank.__balance = 10000000
+print(bank.__balance)
