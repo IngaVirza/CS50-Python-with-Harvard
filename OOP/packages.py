12881dec1165361c37b881931b31087cf9e4f89e
unknown <inha.virza@gmail.com>
2026-07-06 23:50:24 +0300

    @classmethod

--- OOP/packages.py
+++ OOP/packages.py
@@ -10,7 +10,6 @@ def main():
     packages = [
         Package(number=1, sender="Alice", recipient="Bob", weight=10),
         Package(number=2, sender="Bob", recipient="Charlie", weight=5)
-
     ]
 
 
