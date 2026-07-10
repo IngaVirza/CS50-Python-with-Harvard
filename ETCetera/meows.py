7ef16ad59eb90cbc6f3d7653e0d1b6b73bab921b
unknown <inha.virza@gmail.com>
2026-07-11 00:27:22 +0300

    type with sys.argv

--- ETCetera/meows.py
+++ ETCetera/meows.py
@@ -15,9 +15,22 @@
 # cat.meow()
 
 
-def meow(n: int) -> str:
-    return "meow\n" * n
+# def meow(n: int) -> str:
+#     return "meow\n" * n
 
-number: int = int(input("Number: ")   )  
-meows: str = meow(number)
-print(meows, end="")
+# number: int = int(input("Number: ")   )  
+# meows: str = meow(number)
+# print(meows, end="")
+
+
+import sys
+
+if len(sys.argv) == 1:
+    print("meow")
+
+elif len(sys.argv) == 3 and sys.argv[1] == "-n":
+    n = int(sys.argv[2])
+    for _ in range(n):
+        print("meow")    
+else:
+    print("usage: meows.py")    
