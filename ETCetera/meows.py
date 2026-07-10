8eeb6134c4ecb60e55fbae93d5f257e5d08847ce
unknown <inha.virza@gmail.com>
2026-07-10 23:49:56 +0300

    type

--- ETCetera/meows.py
+++ ETCetera/meows.py
@@ -2,14 +2,22 @@
 # for _ in range(MEOWS):
 #     print("meow")
 
-class Cat:
-    MEOWS = 3
+# class Cat:
+#     MEOWS = 3
 
 
-    def meow(self):
-        for _ in range(Cat.MEOWS):
-            print("meow")
+#     def meow(self):
+#         for _ in range(Cat.MEOWS):
+#             print("meow")
 
 
-cat = Cat()
-cat.meow()
+# cat = Cat()
+# cat.meow()
+
+
+def meow(n: int):
+    for _ in range(n):
+        print("meow")
+
+number = input("Number: ")     
+meow(number)
