258afa69f45ca3ede4d7797b5f7bcd66d22810af
unknown <inha.virza@gmail.com>
2026-07-15 01:26:56 +0300

    index

--- ETCetera/students.py
+++ ETCetera/students.py
@@ -7,6 +7,11 @@ students = ["Hermione", "Harry", "Ron"]
 
 # gryffindors = [{"name": student, "house": "Gryffindor"} for student in students]
 
-gryffindors = {student: "Gryffindor" for student in students}
 
-print(gryffindors)    
+
+# gryffindors = {student: "Gryffindor" for student in students}
+# print(gryffindors)    
+
+
+for i in range(len(students)):
+    print(i + 1, students[i])
