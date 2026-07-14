e2b60bac87a46d43a1f8ab014f3db30e736b4cb2
unknown <inha.virza@gmail.com>
2026-07-14 23:39:05 +0300

    filter and for with sorted

--- ETCetera/gryffindors.py
+++ ETCetera/gryffindors.py
@@ -12,10 +12,12 @@ students = [
 # for griffindor in sorted(gryffindors):
 #     print(griffindor)
 
-def is_gryffindor(s):
-    return s ["house"] == "Gryfindor"
+# def is_gryffindor(s):
+#     return s ["house"] == "Gryfindor"
 
-gryffindors = filter(is_gryffindor, students)
+# gryffindors = filter(is_gryffindor, students)
 
-for gryffindor in sorted (gryffindors, key=lambda s: s["name"]):
+gryffindors = filter(lambda s: s["house"] == "Gryffindor", students)
+
+for gryffindor in sorted(gryffindors, key=lambda s: s["name"]):
     print(gryffindor["name"])
