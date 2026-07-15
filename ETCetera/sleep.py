559c8dca85b06431c54f283ee88835f4aaa04d94
unknown <inha.virza@gmail.com>
2026-07-15 23:45:23 +0300

    range with append

--- ETCetera/sleep.py
+++ ETCetera/sleep.py
@@ -1,3 +1,15 @@
-n = int(input("What's n?"))
-for i in range(n):
-    print("*" * i)
+def main():
+    n = int(input("What's n?"))
+    for s in sheep(n):
+        print(s)
+
+def sheep(n):
+    flock = []
+    for i in range(n):
+        flock.append("*" * i)
+
+    return flock    
+
+
+if __name__ == "__main__":
+    main()
